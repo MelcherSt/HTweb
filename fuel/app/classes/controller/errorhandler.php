9f9a5bf16ddd0e2ae53b5b10a2836e5eee8bc5d2
Melcher <melcherst@outlook.com>
2016-12-16 14:06:24 +0100

    Proper 404 status code on 404's

--- fuel/app/classes/controller/errorhandler.php
+++ fuel/app/classes/controller/errorhandler.php
@@ -2,10 +2,10 @@
 /**
  * Routed to by the router configuration. Currently only implementing HttpNotFound (404)
  */
-class Controller_ErrorHandler extends Controller_Base {
+class Controller_ErrorHandler extends \Controller_Base {
 	
-	public function action_404() {		
-		$this->template->title = "Not found";
-		$this->template->content = View::forge('errorhandler/404');
+	public function action_404() {				
+		$content = View::forge('errorhandler/404');
+		return \Response::forge(\View::forge('template', ['title' => __('not_found'), 'page_title' => __('not_found'), 'content' => $content]), 404);
 	}
 }
