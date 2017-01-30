9183018aa6525c4592f55a43f11ce867808a1406
Melcher <melcherst@outlook.com>
2017-01-30 22:25:30 +0100

    Improve paginated rest api

--- fuel/app/modules/api/classes/controller/v1/sessions.php
+++ fuel/app/modules/api/classes/controller/v1/sessions.php
@@ -8,6 +8,38 @@ class Controller_v1_Sessions extends Controller_RestPaginated {
 		
 	}
 	
+	public function action_bycook($user_id) {
+		$array = \Sessions\Model_Session::query()
+				->where('settled', 0)
+				->related('enrollments')
+				->where('enrollments.user_id', $user_id)
+				->rows_offset($this->offset)
+				->rows_limit($this->limit)
+				->order_by('date', $this->order)
+				->get();
+		
+		return array_map(function($item) {
+				return new \Sessions\Dto_SessionListItem($item);
+			}, $array);
+		
+	}
+	
+	public function action_byuser($user_id) {
+		$array = \Sessions\Model_Session::query()
+				->where('settled', 0)
+				->related('enrollments')
+				->where('enrollments.user_id', $user_id)
+				->rows_offset($this->offset)
+				->rows_limit($this->limit)
+				->order_by('date', $this->order)
+				->get();
+		
+		return array_map(function($item) {
+				return new \Sessions\Dto_SessionListItem($item);
+			}, $array);
+
+	}
+	
 	public function action_admin() {
 		
 		$array = \Sessions\Model_Session::query()
@@ -17,14 +49,18 @@ class Controller_v1_Sessions extends Controller_RestPaginated {
 				->order_by('date', $this->order)
 				->get();
 	
-		array_map(function($item) {
+		return array_map(function($item) {
 				return new \Sessions\Dto_SessionListItem($item);
 			}, $array);
 		
-		return [
+	}
+	
+	public function after($array) {
+		
+		return parent::after([
 			'total' => sizeof($array),
 			'rows' => array_values($array)
-		];
+		]);
 	}
 	
 	
