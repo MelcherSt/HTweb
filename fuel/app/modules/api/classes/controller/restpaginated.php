0a785c2a882e0a8c97778dd04af7fad4da99ca7b
Melcher <melcherst@outlook.com>
2017-01-31 00:17:20 +0100

    Move pagination to separate function

--- fuel/app/modules/api/classes/controller/restpaginated.php
+++ fuel/app/modules/api/classes/controller/restpaginated.php
@@ -8,16 +8,41 @@ class Controller_RestPaginated extends Controller_RestAuth {
 	protected $limit = null;
 	protected $order = 'asc';
 			
+	
 	function before() {
 		parent::before();
 		
-		// Set pagination values
+		/* 
+		 * Set pagination values. Note that \Fuel\Orm\Query has been changed
+		 * to correctly deal with null values for row offset and row limit.
+		 */		
 		$this->offset = \Input::get('offset');
 		$this->limit = \Input::get('limit');
 		
+		// Only set order when appropriate
 		$order_param = \Input::get('order');
-		if($order_param == 'desc' || $order_param == 'asc'){
+		if(in_array($order_param, ['asc', 'desc'])){
 			$this->order =$order_param;
 		}	
+	}
+	
+	/**
+	 * Subsequently paginate and execute query
+	 * @param \Orm\Query $query
+	 * @return array
+	 */
+	protected final function paginate_query(\Orm\Query $query) : array {
+		return $query->rows_offset($this->offset)
+			->rows_limit($this->limit)
+			->order_by('date', $this->order)
+			->get();
+	}
+	
+	public final function after($array) : \Response {
+		// Wrap return body
+		return parent::after([
+			'total' => sizeof($array),
+			'rows' => array_values($array)
+		]);
 	}
 }
--- fuel/app/modules/api/classes/controller/v1/sessions.php
+++ fuel/app/modules/api/classes/controller/v1/sessions.php
@@ -9,14 +9,10 @@ class Controller_v1_Sessions extends Controller_RestPaginated {
 	}
 	
 	public function action_bycook($user_id) {
-		$array = \Sessions\Model_Session::query()
+		$array = $this->paginate_query(\Sessions\Model_Session::query()
 				->where('settled', 0)
 				->related('enrollments')
-				->where('enrollments.user_id', $user_id)
-				->rows_offset($this->offset)
-				->rows_limit($this->limit)
-				->order_by('date', $this->order)
-				->get();
+				->where('enrollments.user_id', $user_id));
 		
 		return array_map(function($item) {
 				return new \Sessions\Dto_SessionListItem($item);
@@ -25,14 +21,10 @@ class Controller_v1_Sessions extends Controller_RestPaginated {
 	}
 	
 	public function action_byuser($user_id) {
-		$array = \Sessions\Model_Session::query()
+		$array = $this->paginate_query(\Sessions\Model_Session::query()
 				->where('settled', 0)
 				->related('enrollments')
-				->where('enrollments.user_id', $user_id)
-				->rows_offset($this->offset)
-				->rows_limit($this->limit)
-				->order_by('date', $this->order)
-				->get();
+				->where('enrollments.user_id', $user_id));
 		
 		return array_map(function($item) {
 				return new \Sessions\Dto_SessionListItem($item);
@@ -42,27 +34,13 @@ class Controller_v1_Sessions extends Controller_RestPaginated {
 	
 	public function action_admin() {
 		
-		$array = \Sessions\Model_Session::query()
-				->where('settled', 0)
-				->rows_offset($this->offset)
-				->rows_limit($this->limit)
-				->order_by('date', $this->order)
-				->get();
+		$array = $this->paginate_query(\Sessions\Model_Session::query()
+				->where('settled', 0));
 	
 		return array_map(function($item) {
 				return new \Sessions\Dto_SessionListItem($item);
 			}, $array);
 		
 	}
-	
-	public function after($array) {
-		
-		return parent::after([
-			'total' => sizeof($array),
-			'rows' => array_values($array)
-		]);
-	}
-	
-	
 }
 
