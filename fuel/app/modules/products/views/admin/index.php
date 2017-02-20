acf1705758bff6be99be84ae2125bb48590bfd9c
melcher <melcherst@outlook.com>
2017-02-20 13:00:51 +0100

    Add additional column to products admin

--- fuel/app/modules/products/views/admin/index.php
+++ fuel/app/modules/products/views/admin/index.php
@@ -15,7 +15,8 @@
 				<thead>
 					<tr>
 						<th class="col-md-2"><?=__('product.field.name')?></th>
-						<th class="col-md-2"><?=__('product.field.date')?></th>		
+						<th class="col-md-2"><?=__('product.field.date')?></th>	
+						<th class="col-md-2"><?=__('product.field.paid_by')?></th>
 						<th class="col-md-2"><?=__('product.field.participant_plural')?></th>
 						<th class="col-md-1"><?=__('product.field.cost')?></th>
 						<th class="col-md-2"><?=__('actions.name')?></th>
@@ -26,6 +27,7 @@
 					<tr class="clickable-row" data-href="/products/view/<?=$product->id?>">
 						<td><?=$product->name?></td>
 						<td><?=date('Y-m-d', $product->created_at)?></td>
+						<td><?=$product->get_payer()->get_fullname()?>
 						<td><?=$product->get_nicified_participants()?></td>
 						<td><?='€ ' . $product->cost?></td>
 						<td><a href="#" data-href="#" class="clickable-row" onclick="showDeleteModal(<?=$product->id?>, '<?=e($product->name)?>')"><span class="fa fa-close"></span> <?=__('actions.remove')?></a></td>
