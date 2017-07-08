af7d2db082568f1749073fe307090385709db962
Melcher <melcherst@outlook.com>
2017-07-08 22:48:33 +0200

    make date nullable for a smooth transition

--- fuel/app/modules/dashboard/views/index.php
+++ fuel/app/modules/dashboard/views/index.php
@@ -1,11 +1,11 @@
 <!-- Widget row -->
 <div class="row">
-//<?php foreach($widgets as $widget) {
-//	echo Request::forge($widget)->execute();
-//} ?>
+<?php foreach($widgets as $widget) {
+	echo Request::forge($widget)->execute();
+} ?>
 </div>
-<!-- .Widget row -->
 
+<!-- widget row -->
 <div class="alert alert-info">
 	<strong><?=__('alert.call.attention')?></strong>	
 	<?=__('alert.msg.alpha_rel', ['product' => __('site_title') . ' ' .__('site_sub')])?>
--- fuel/app/modules/products/migrations/003_add_product_date.php
+++ fuel/app/modules/products/migrations/003_add_product_date.php
@@ -7,7 +7,7 @@ class Add_Product_Date
 	public function up()
 	{
 		\DBUtil::add_fields('products', [
-			'date' => ['type' => 'date', 'null' => false]
+			'date' => ['type' => 'date']
 		]);
 	}
 
