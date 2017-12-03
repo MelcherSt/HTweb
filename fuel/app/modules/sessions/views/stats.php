61424ef1aed2f4397b42f541e8a5a47cb028bcbd
Melcher <melcherst@outlook.com>
2017-12-03 15:51:45 +0100

    Round enrollments and fix columns

--- fuel/app/modules/sessions/views/stats.php
+++ fuel/app/modules/sessions/views/stats.php
@@ -9,39 +9,38 @@
 <div class="row">
 	<h5 class="pull-left"><?= __('session.stats.next_cook_msg') . ': ' . $next_cook ?></h5>
 
-<?php
-$server = $checksum['server'];
-$local = $checksum['local'];
-?>
-
-<div class="pull-right">
-<?php if ($server == $local && $server < 11) :
+	<?php
+	$server = $checksum['server'];
+	$local = $checksum['local'];
 	?>
-	<p class="label label-success"><span class="fa fa-check"></span> <?= __('receipt.view.point_check') ?> (<?= $server ?>)</p>
-<?php else : ?>
-	<p class="label label-danger"><span class="fa fa-times"></span> <?= __('receipt.view.point_check') ?> (<?= $local . '/' . $server ?>)</p>
-<?php endif; ?>
-</div>
+
+	<div class="pull-right">
+		<?php if ($server == $local && $server < 11) : ?>
+			<p class="label label-success"><span class="fa fa-check"></span> <?= __('receipt.view.point_check') ?> (<?= $server ?>)</p>
+		<?php else : ?>
+			<p class="label label-danger"><span class="fa fa-times"></span> <?= __('receipt.view.point_check') ?> (<?= $local . '/' . $server ?>)</p>
+		<?php endif; ?>
+	</div>
 </div>
 
-<h3><?= __('session.stats.all_time').' '. strtolower(__('session.stats.title')) ?></h3>
+<h3><?= __('session.stats.all_time') . ' ' . strtolower(__('session.stats.title')) ?></h3>
 <div class="table-responsive">
 	<table class="table table-striped table-hover table-condensed">
 		<thead>
 			<tr>
-				<th class="col-md-2"><?= __('session.role.cook_plural') ?></th>
-				<th class="col-md-1"><?= __('session.stats.average').' '.__('product.field.cost') ?></th>
-				<th class="col-md-1">#</th>
-				<th class="col-md-1"><?= __('session.stats.average').' '.__('session.role.participant_plural')?></th>
+				<th><?= __('session.role.cook') ?></th>
+				<th>#</th>
+				<th><?= __('session.stats.average') . ' ' . __('product.field.cost') ?></th>
+				<th><?= __('session.stats.average') . ' ' . __('session.role.participant_plural') ?></th>
 			</tr>
 		</thead>
 		<tbody>
 			<?php foreach (\Sessions\Model_Session::get_avg_cook_costs() as $entry) { ?>
 				<tr class="clickable-row" data-href="#">
-					<td><?= $entry['name']?></td>
-					<td><?= '€ ' . round($entry['avg_cost'], 2) ?></td>
+					<td><?= $entry['name'] ?></td>
 					<td><?= $entry['count'] ?></td>
-					<td><?= $entry['avg_enrollments'] ?></td>
+					<td><?= '€ ' . round($entry['avg_cost'], 2) ?></td>			
+					<td><?= round($entry['avg_enrollments']) ?></td>
 				</tr>
 			<?php } ?>
 		</tbody>	
